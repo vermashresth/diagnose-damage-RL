29aa4f8fb175f3e9057a85dea90cea3d5874d93d
Shresth Verma <vermashresth@gmail.com>
2018-08-31 03:27:57 +0530

    efficiency

--- rnn_test.py
+++ rnn_test.py
@@ -3,19 +3,19 @@ import pickle
 import numpy as np
 def create_model():
 
-    model = load_model("saved_models/my_modelant4jointsday20_datadiff20timerandpol1.h5")
+    model = load_model("saved_models/my_modelant4jointsday31_eff_div.h5")
     print("loading samples")
-    pickle_in = open("data_pickles/Ant-v1_4joints20diff5000randpoljamtest1.dict", "rb")
+    pickle_in = open("data_pickles/Ant-v1_4joints20diff100doversetestshort1.dict", "rb")
     data = pickle.load(pickle_in)
     print("samples loaded")
     print(data['class'].shape)
     pred = model.predict(data['bigdata1'])
-    print pred[:10]
+    #print pred[:10]
     a=np.argmax(pred, axis=1)
-    print a[:10]
+    print a[:300]
     #print(data['y_data'].reshape(80,6)[::5])
     b=data['class'].flatten()
-    print b[:10]
+    print b[:300]
     print np.sum(a==b), len(b)
     print (float(np.sum(a==b))/float(len(b)))
 
